08b42ec2a72a5c0b2770a55a5371c52e9cc0e58e
Sean Fox <seandfox@proton.me>
2024-05-06 17:06:07 -0400

    fixed mobile browser responsiveness issues

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,21 +4,21 @@ import video from "/src/assets/herovid0.WEBM";
 const Hero = () => {
   return (
     <div>
-      <div className="absolute z-10 md:max-w-[1480px] grid md:grid-cols-1 max-w-[600px] justify-items-center px-24 py-48">
-        <div className="min-h-[250px] flex mt-10 sm:mt-0">
-          <div className="grid sm:grid-cols-1 gap-6">
+      <div className="absolute z-10 md:max-w-[1480px] grid md:grid-cols-1 max-w-[600px] justify-items-center">
+        <div className="min-h-[250px] flex mt-4 sm:mt-0">
+          <div className="gap-6">
             {/* Text section */}
-            <div className="space-y-5 md:flex font-worksans">
+            <div className="space-y-5 md:flex font-worksans flex justify-center flex-col">
               <div>
-                <h1 className="text-white text-6xl pr-12 sm:pt-12">
-                  PES Pacific Environmental Solutions
+                <h1 className="text-white text-4xl p-12 flex justify-center">
+                  Pacific Environmental Solutions
                 </h1>
               </div>
 
-              <div className="text-white bg-gray-700 bg-opacity-40 px-8 py-8 rounded-xl my-8">
+              <div className="text-white bg-gray-700 bg-opacity-40 p-8 rounded-xl m-8">
                 <h1
                   // data-aos="fade-up"
-                  className="text-4xl sm:text-4xl text-white "
+                  className="text-2xl md:text-4xl text-white "
                   style={{ lineHeight: 1.2 }}
                 >
                   We combine innovation, sustainability and cultural respect to{" "}
