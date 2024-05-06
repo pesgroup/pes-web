8d8c2c2b9efbb94448e32d7068e1215222722a79
Sean Fox <seandfox@proton.me>
2024-05-05 22:03:28 -0400

    changed base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pes-web/",
+  base: "/",
   plugins: [react()],
   assetsInclude: ["**/*.WEBM", "**/*.PNG"],
   resolve: {
