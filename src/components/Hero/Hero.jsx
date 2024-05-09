27c7400cf177b08336736eda04ed00a55a604b95
Sean Fox <seandfox@proton.me>
2024-05-09 18:48:04 -0400

    fixed hero styles

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,26 +3,21 @@ import video from "/src/assets/herovid1.MP4";
 
 const Hero = () => {
   return (
-    <section className="relative h-screen w-full">
-      <div className="absolute">
+    <section className="relative h-screen align-middle">
+      <div className="absolute w-full h-full overlfow-hidden">
         <video className="object-cover w-full h-screen" autoPlay loop>
           <source src={video} type="video/MP4" preload="auto" />
         </video>
       </div>
       <div
-        className="flex flex-col justify-center items-center align-middle font-worksans md:p-48"
+        className="z-10 flex flex-col justify-center items-center align-middle font-worksans"
         data-aos="fade-up"
       >
-        <div className="flex">
-          {/* <h1 className="text-white md:text-5xl text-4xl text-left font-normal">
-            Pacific Environmental Solutions
-          </h1> */}
-        </div>
-        <div className="flex justify-center">
-          <div className="text-gray-100 bg-gray-800 bg-opacity-60 p-6 rounded-md text-left leading-5 md:w-[400px] mt-4">
-             <h1 className="text-yellow-300 md:text-5xl text-5xl text-left font-medium my-2">
-            Pacific Environmental Solutions
-          </h1>
+        <div className="relative z-10 flex mt-12 md:mt-24">
+          <div className="text-gray-100 bg-gray-800 bg-opacity-60 p-8 rounded-md text-left leading-5 md:w-[350px] mt-4">
+            <h1 className="text-yellow-300  text-4xl text-left font-medium my-2">
+              Pacific Environmental Solutions
+            </h1>
             <h2 className="text-lg text-white">
               We combine innovation, sustainability and cultural respect to{" "}
               <span className="text-yellow-300 font-medium">
